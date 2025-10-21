f3cb5f8266dde8178b0d3701a915c1d294b78e3f
shiggityshwah <stefanom@gmail.com>
2025-10-21 10:30:37 -0700

    Set calculator window height to 715px
    Previous height of 680px was too short and caused scrollbars on all tabs

--- menu.js
+++ menu.js
@@ -116,7 +116,7 @@
                     url: url,
                     type: 'popup',
                     width: 400,
-                    height: Math.min(680, 800)
+                    height: 715
                 });
             });
 
